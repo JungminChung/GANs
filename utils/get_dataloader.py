66632c117861899e31a5693449b71ca6c979018b
jungminchung <mmismin00@gmail.com>
2021-05-12 21:04:16 +0900

    Fix : typo error

--- utils/get_dataloader.py
+++ utils/get_dataloader.py
@@ -4,14 +4,14 @@ from torch.utils.data import DataLoader
 
 def get_dataloader(dataset, args):
     transform_img = transforms.Compose([
-                        transforms.Resize(size=self.image_size, interpolation=0),
+                        transforms.Resize(size=args.img_size, interpolation=0),
                         transforms.ToTensor(),
                         transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
                     ])
     
     if dataset == 'mnist': 
-        dataset = dataset.MNIST(root='./DATA', 
-                                trian=True,
+        dataset = datasets.MNIST(root='./DATA', 
+                                train=True,
                                 transform=transform_img,
                                 download=True)
     elif dataset == 'fashionmnist':
